b16dda7e2cab1b5f6db73309275a1d2d7ae2aafd
vahagn <vahag.kostanyan974@gmail.com>
2024-07-11 14:52:22 +0400

    correct timestemps

--- helpers/currentDateInArmenia.js
+++ helpers/currentDateInArmenia.js
@@ -1,17 +1,10 @@
 const currentDateInArmenia = () => {
-
-  let currentDate = new Date();
-
-  // Convert to Armenia Time Zone (UTC+4)
-  let options = { timeZone: 'Asia/Yerevan', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
-  let armeniaDate = currentDate.toLocaleString('en-US', options);
-
-  let date = new Date(armeniaDate);
-
-  // Get the Unix timestamp in milliseconds
-  let unixTimestamp = date.getTime();
-
-  return unixTimestamp;
+  const currentDate = new Date();
+  const utcTimestamp = currentDate.getTime(); 
+  const armeniaOffset = 4 * 3600000; 
+  const armeniaTimestamp = utcTimestamp + armeniaOffset;
+  
+  return armeniaTimestamp;
 }
 
 module.exports = {currentDateInArmenia};
